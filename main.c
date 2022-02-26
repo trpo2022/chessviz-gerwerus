5b950a5efe268d0a03bf4cd6679d5d164e3b4475
= <prosyannikov1978@mail.ru>
2022-02-26 22:28:35 +0700

    Delete Global Variable, Remove Magic Numbers

--- main.c
+++ main.c
@@ -1,28 +1,27 @@
-#include "stdio.h"
-
+#include <stdio.h>
 #define deskSide 8
-char Desk[deskSide][deskSide] = {
-	{'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
-	{'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-	{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-	{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-	{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-	{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-	{'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-	{'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'}};
-void PrintDesk(char Desk[8][8]);
 
+void PrintDesk(char Desk[deskSide][deskSide]);
 int main()
 {
+	char Desk[deskSide][deskSide] = {
+		{'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
+		{'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+		{'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+		{'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'}};
 	PrintDesk(Desk);
 }
 
-void PrintDesk(char Desk[8][8])
+void PrintDesk(char Desk[deskSide][deskSide])
 {
 	int i;
-	for(i = 0; i < deskSide; i++)
+	for (i = 0; i < deskSide; i++)
 	{
-		printf("%d", 8 - i);
+		printf("%d", deskSide - i);
 		int j;
 		for (j = 0; j < deskSide; j++)
 		{
