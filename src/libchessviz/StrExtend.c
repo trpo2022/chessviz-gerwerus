1f95c36845e52726135c2731d72af60c0b8915f1
Герман Просянников <100204107+gerwerus@users.noreply.github.com>
2022-04-22 20:09:44 +0700

    Update StrExtend.c

--- src/libchessviz/StrExtend.c
+++ src/libchessviz/StrExtend.c
@@ -1,1 +1,16 @@
+#include <libchessviz/StrExtend.h>
 
+int IsUpper(char c) {
+    if (c == tolower(c))
+        return -1;
+    return 1;
+}
+int strfnd(char* dest, char val) {
+    size_t i = 0;
+    while (i < strlen(dest)) {
+        if (dest[i] == val)
+            return i;
+        i++;
+    }
+    return -1;
+}
