4fc6017ef023f473413043ccc7e9535cfe5ae15e
Герман Просянников <100204107+gerwerus@users.noreply.github.com>
2022-04-22 20:07:51 +0700

    Update Get.h

--- src/libchessviz/Get.h
+++ src/libchessviz/Get.h
@@ -1,1 +1,21 @@
+#include <libchessviz/StrExtend.h>
+#include <ctype.h>
+#include <string.h>
+#include <libchessviz/Check.h>
+#define CallSize 10
+#define WHITE 0
+#define BLACK 1
 
+#pragma once
+
+typedef struct Move {
+    int H0;
+    int V0;
+    int H1;
+    int V1;
+    char MoveType;
+} Move;
+
+char GetFigure(char Call[], int Color);
+Move GetMove(char Call[]);
+Right GetRight(char Figure, Move move, char Desk[][deskSide]);
