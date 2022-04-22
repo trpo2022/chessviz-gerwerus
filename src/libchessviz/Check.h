68eb2798ac660fd69a525c2529d37633653f7ad2
Герман Просянников <100204107+gerwerus@users.noreply.github.com>
2022-04-22 20:07:13 +0700

    Update Check.h

--- src/libchessviz/Check.h
+++ src/libchessviz/Check.h
@@ -1,1 +1,12 @@
+#include <libchessviz/StrExtend.h>
 
+#define deskSide 8
+#pragma once
+typedef struct Right {
+    int Code;
+    char Error[100];
+} Right;
+
+Right CheckPawn(
+        int H0, int H1, int V0, int V1, char MoveType, int Figure,
+        char Desk[][deskSide]);
