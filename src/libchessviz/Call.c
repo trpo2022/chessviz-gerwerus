651ab8a47df52b2c9b85cfb6647cffa3b7fb9883
Герман Просянников <100204107+gerwerus@users.noreply.github.com>
2022-04-24 00:23:15 +0700

    Update Call.c

--- src/libchessviz/Call.c
+++ src/libchessviz/Call.c
@@ -1,6 +1,6 @@
 #include <libchessviz/Call.h>
 void MakeCall(Move move, char Figure, char Desk[][deskSide])
 {
-	Desk[move.V0][move.H0] = ' ';
+    Desk[move.V0][move.H0] = ' ';
     Desk[move.V1][move.H1] = Figure;
 }
