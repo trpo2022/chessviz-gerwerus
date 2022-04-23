2ced11ebe09b58148d0cb58b909af2e74fcd895f
Герман Просянников <100204107+gerwerus@users.noreply.github.com>
2022-04-23 15:05:25 +0700

    Code Formating

--- src/libchessviz/PrintDesk.c
+++ src/libchessviz/PrintDesk.c
@@ -1,11 +1,14 @@
 #include <libchessviz/PrintDesk.h>
 
-void PrintDesk(char Desk[deskSide][deskSide]){
+void PrintDesk(char Desk[deskSide][deskSide])
+{
     int i;
-    for (i = 0; i < deskSide; i++) {
+    for (i = 0; i < deskSide; i++)
+    {
         printf("%d", deskSide - i);
         int j;
-        for (j = 0; j < deskSide; j++) {
+        for (j = 0; j < deskSide; j++)
+        {
             printf(" %c", Desk[i][j]);
         }
         printf("\n");
