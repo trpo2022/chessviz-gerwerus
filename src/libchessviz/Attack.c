6b99a4d53ea6717cdf9dcf87fa26f37bd8aebdfc
Герман Просянников <100204107+gerwerus@users.noreply.github.com>
2022-04-23 23:58:24 +0700

    Update Attack.c

--- src/libchessviz/Attack.c
+++ src/libchessviz/Attack.c
@@ -4,174 +4,239 @@
 int UnderAttack(int X, int Y, char Desk[][deskSide])
 {
 	int h = Y, v = X, WhiteAttack = 0, BlackAttack = 0;
-	
-	for(h += 1;h < deskSide; h++) 
-		if(Desk[v][h] != ' ' && h != Y) {
-			
-			if(Desk[v][h] == 'R') WhiteAttack = 1;
-			else if(Desk[v][h] == 'Q') WhiteAttack = 1;
-			else if(Desk[v][h] == 'K' && abs(Y - h) == 1) WhiteAttack = 1;
-			
-			if(Desk[v][h] == 'r') BlackAttack = 1;
-			else if(Desk[v][h] == 'q') BlackAttack = 1;
-			else if(Desk[v][h] == 'k' && abs(Y - h) == 1) BlackAttack = 1;
-			
-			break; 	
-		}
 
-	h = Y; 
-	v = X;
-	for(h -= 1; h >= 0; h--) 
-		if(Desk[v][h] != ' ' && h != Y) {		
-			if(Desk[v][h] == 'R') WhiteAttack = 1;
-			else if(Desk[v][h] == 'Q') WhiteAttack = 1;
-			else if(Desk[v][h] == 'K' && abs(Y - h) == 1) WhiteAttack = 1;
-			
-		
-			if(Desk[v][h] == 'r') BlackAttack = 1;
-			else if(Desk[v][h] == 'q') BlackAttack = 1;
-			else if(Desk[v][h] == 'k' && abs(Y - h) == 1) BlackAttack = 1;
-			
-			break;
-		}
-	
-	h = Y; 
-	v = X;
-	for(v += 1;h < deskSide; v++) 
-		if(Desk[v][h] != ' ' && v != X) {
-			
-			if(Desk[v][h] == 'R') WhiteAttack = 1;
-			else if(Desk[v][h] == 'Q') WhiteAttack = 1;
-			else if(Desk[v][h] == 'K' && abs(Y - h) == 1) WhiteAttack = 1;
-			
-			if(Desk[v][h] == 'r') BlackAttack = 1;
-			else if(Desk[v][h] == 'q') BlackAttack = 1;
-			else if(Desk[v][h] == 'k' && abs(Y - h) == 1) BlackAttack = 1;
-			
-			break; 	
-		}
+    for (h += 1; h < deskSide; h++)
+        if (Desk[v][h] != ' ' && h != Y) {
+            if (Desk[v][h] == 'R')
+                WhiteAttack = 1;
+            else if (Desk[v][h] == 'Q')
+                WhiteAttack = 1;
+            else if (Desk[v][h] == 'K' && abs(Y - h) == 1)
+                WhiteAttack = 1;
 
-	h = Y; 
-	v = X;
-	for(v -= 1; v >= 0; v--) 
-		if(Desk[v][h] != ' ' && v != X) {		
-			if(Desk[v][h] == 'R') WhiteAttack = 1;
-			else if(Desk[v][h] == 'Q') WhiteAttack = 1;
-			else if(Desk[v][h] == 'K' && abs(Y - h) == 1) WhiteAttack = 1;
-			
-		
-			if(Desk[v][h] == 'r') BlackAttack = 1;
-			else if(Desk[v][h] == 'q') BlackAttack = 1;
-			else if(Desk[v][h] == 'k' && abs(Y - h) == 1) BlackAttack = 1;
-			
-			break;
-		}
-	h = Y, v = X;
-	while(h < deskSide && v < deskSide) {
-		
-		if(Desk[v][h] != ' ' && v != X && h != Y){
-			if(Desk[v][h] == 'Q') WhiteAttack = 1;
-			else if(Desk[v][h] == 'B') WhiteAttack = 1;
-			else if(Desk[v][h] == 'P' && X - v == -1 && Y - h == -1) WhiteAttack = 1;
-			else if(Desk[v][h] == 'K' && abs(X - v) == 1 && abs(Y - h) == 1) WhiteAttack = 1;
-			
-			if(Desk[v][h] == 'q') BlackAttack = 1;
-			else if(Desk[v][h] == 'b') BlackAttack = 1;
-			else if(Desk[v][h] == 'k' && abs(X - v) == 1 && abs(Y - h) == 1) BlackAttack = 1;
-			
-			break;	
-		}
-		h++; v++;
-	}
-	h = Y, v = X;
-	while(h >= 0 && v >= 0){	
-		if(Desk[v][h] != ' ' && v != X && h != Y) {
-			if(Desk[v][h] == 'Q') WhiteAttack = 1;
-			else if(Desk[v][h] == 'B') WhiteAttack = 1;
-			else if(Desk[v][h] == 'K' && abs(X - v) == 1 && abs(Y - h) == 1) WhiteAttack = 1;
-			
-			if(Desk[v][h] == 'q') BlackAttack = 1;
-			else if(Desk[v][h] == 'b') BlackAttack = 1;
-			else if(Desk[v][h] == 'p' && X - v == 1 && Y - h == 1) BlackAttack = 1;
-			else if(Desk[v][h] == 'k' && abs(X - v) == 1 && abs(Y - h) == 1) BlackAttack = 1;
-			
-			break;	
-		}
-		h--; 
-		v--;
-	}
-	
-	h = Y, v = X;
-	while(h >= 0 && v < deskSide) {
-		
-		if(Desk[v][h] != ' ' && v != X && h != Y) {
-			if(Desk[v][h] == 'Q') WhiteAttack = 1;
-			else if(Desk[v][h] == 'B') WhiteAttack = 1;
-			else if(Desk[v][h] == 'P' && X - v == -1 && Y - h == 1) WhiteAttack = 1;
-			else if(Desk[v][h] == 'K' && abs(X - v) == 1 && abs(Y - h) == 1) WhiteAttack = 1;
-			
-			if(Desk[v][h] == 'q') BlackAttack = 1;
-			else if(Desk[v][h] == 'b') BlackAttack = 1;
-			else if(Desk[v][h] == 'k' && abs(X - v) == 1 && abs(Y - h) == 1) BlackAttack = 1;
-			
-			break;	
-		}
-		h--;
-		v++;
-	}
-	h = Y, v = X;
-	while(h < deskSide && v >= 0) {	
-		if(Desk[v][h] != ' ' && v != X && h != Y){
-			if(Desk[v][h] == 'Q') WhiteAttack = 1;
-			else if(Desk[v][h] == 'B') WhiteAttack = 1;
-			else if(Desk[v][h] == 'K' && abs(X - v) == 1 && abs(Y - h) == 1) WhiteAttack = 1;
-			
-			if(Desk[v][h] == 'q') BlackAttack = 1;
-			else if(Desk[v][h] == 'b') BlackAttack = 1;
-			else if(Desk[v][h] == 'p' && X - v == 1 && Y - h == -1) BlackAttack = 1;
-			else if(Desk[v][h] == 'k' && abs(X - v) == 1 && abs(Y - h) == 1) BlackAttack = 1;
-			
-			break;	
-		}
-		h++; 
-		v--;
-	}
-	if(X - 2 >= 0 && Y - 1 >= 0) {
-		if(Desk[X - 2][Y - 1] == 'N') WhiteAttack = 1;
-		else if(Desk[X - 2][Y - 1] == 'n') BlackAttack = 1;
-	}
-	if(X + 2 < deskSide && Y - 1 >= 0) {
-		if(Desk[X + 2][Y - 1] == 'N') WhiteAttack = 1;
-		else if(Desk[X + 2][Y - 1] == 'n') BlackAttack = 1;
-	}
-	if(X - 2 >= 0 && Y + 1 < deskSide) {
-		if(Desk[X - 2][Y + 1] == 'N') WhiteAttack = 1;
-		else if(Desk[X - 2][Y + 1] == 'n') BlackAttack = 1;
-	}
-	if(X + 2 < deskSide && Y + 1 < deskSide) {
-		if(Desk[X + 2][Y + 1] == 'N') WhiteAttack = 1;
-		else if(Desk[X + 2][Y + 1] == 'n') BlackAttack = 1;
-	}
-	
-	if(X - 1 >= 0 && Y - 2 >= 0) {
-		if(Desk[X - 1][Y - 2] == 'N') WhiteAttack = 1;
-		else if(Desk[X - 1][Y - 2] == 'n') BlackAttack = 1;
-	}
-	if(X + 1 < deskSide && Y - 2 >= 0) {
-		if(Desk[X + 1][Y - 2] == 'N') WhiteAttack = 1;
-		else if(Desk[X + 1][Y - 2] == 'n') BlackAttack = 1;
-	}
-	if(X - 1 >= 0 && Y + 2 < deskSide) {
-		if(Desk[X - 1][Y + 2] == 'N') WhiteAttack = 1;
-		else if(Desk[X - 1][Y + 2] == 'n') BlackAttack = 1;
-	}
-	if(X + 1 < deskSide && Y + 2 < deskSide) {
-		if(Desk[X + 1][Y + 2] == 'N') WhiteAttack = 1;
-		else if(Desk[X + 1][Y + 2] == 'n') BlackAttack = 1;
-	}
-	if(BlackAttack == 0) return WhiteAttack;
-	if(WhiteAttack == 0) return -BlackAttack;
-	return WhiteAttack + BlackAttack;
+            if (Desk[v][h] == 'r')
+                BlackAttack = 1;
+            else if (Desk[v][h] == 'q')
+                BlackAttack = 1;
+            else if (Desk[v][h] == 'k' && abs(Y - h) == 1)
+                BlackAttack = 1;
+
+            break;
+        }
+
+    h = Y;
+    v = X;
+    for (h -= 1; h >= 0; h--)
+        if (Desk[v][h] != ' ' && h != Y) {
+            if (Desk[v][h] == 'R')
+                WhiteAttack = 1;
+            else if (Desk[v][h] == 'Q')
+                WhiteAttack = 1;
+            else if (Desk[v][h] == 'K' && abs(Y - h) == 1)
+                WhiteAttack = 1;
+
+            if (Desk[v][h] == 'r')
+                BlackAttack = 1;
+            else if (Desk[v][h] == 'q')
+                BlackAttack = 1;
+            else if (Desk[v][h] == 'k' && abs(Y - h) == 1)
+                BlackAttack = 1;
+
+            break;
+        }
+
+    h = Y;
+    v = X;
+    for (v += 1; h < deskSide; v++)
+        if (Desk[v][h] != ' ' && v != X) {
+            if (Desk[v][h] == 'R')
+                WhiteAttack = 1;
+            else if (Desk[v][h] == 'Q')
+                WhiteAttack = 1;
+            else if (Desk[v][h] == 'K' && abs(Y - h) == 1)
+                WhiteAttack = 1;
+
+            if (Desk[v][h] == 'r')
+                BlackAttack = 1;
+            else if (Desk[v][h] == 'q')
+                BlackAttack = 1;
+            else if (Desk[v][h] == 'k' && abs(Y - h) == 1)
+                BlackAttack = 1;
+
+            break;
+        }
+
+    h = Y;
+    v = X;
+    for (v -= 1; v >= 0; v--)
+        if (Desk[v][h] != ' ' && v != X) {
+            if (Desk[v][h] == 'R')
+                WhiteAttack = 1;
+            else if (Desk[v][h] == 'Q')
+                WhiteAttack = 1;
+            else if (Desk[v][h] == 'K' && abs(Y - h) == 1)
+                WhiteAttack = 1;
+
+            if (Desk[v][h] == 'r')
+                BlackAttack = 1;
+            else if (Desk[v][h] == 'q')
+                BlackAttack = 1;
+            else if (Desk[v][h] == 'k' && abs(Y - h) == 1)
+                BlackAttack = 1;
+
+            break;
+        }
+    h = Y, v = X;
+    while (h < deskSide && v < deskSide) {
+        if (Desk[v][h] != ' ' && v != X && h != Y) {
+            if (Desk[v][h] == 'Q')
+                WhiteAttack = 1;
+            else if (Desk[v][h] == 'B')
+                WhiteAttack = 1;
+            else if (Desk[v][h] == 'P' && X - v == -1 && Y - h == -1)
+                WhiteAttack = 1;
+            else if (Desk[v][h] == 'K' && abs(X - v) == 1 && abs(Y - h) == 1)
+                WhiteAttack = 1;
+
+            if (Desk[v][h] == 'q')
+                BlackAttack = 1;
+            else if (Desk[v][h] == 'b')
+                BlackAttack = 1;
+            else if (Desk[v][h] == 'k' && abs(X - v) == 1 && abs(Y - h) == 1)
+                BlackAttack = 1;
+
+            break;
+        }
+        h++;
+        v++;
+    }
+    h = Y, v = X;
+    while (h >= 0 && v >= 0) {
+        if (Desk[v][h] != ' ' && v != X && h != Y) {
+            if (Desk[v][h] == 'Q')
+                WhiteAttack = 1;
+            else if (Desk[v][h] == 'B')
+                WhiteAttack = 1;
+            else if (Desk[v][h] == 'K' && abs(X - v) == 1 && abs(Y - h) == 1)
+                WhiteAttack = 1;
+
+            if (Desk[v][h] == 'q')
+                BlackAttack = 1;
+            else if (Desk[v][h] == 'b')
+                BlackAttack = 1;
+            else if (Desk[v][h] == 'p' && X - v == 1 && Y - h == 1)
+                BlackAttack = 1;
+            else if (Desk[v][h] == 'k' && abs(X - v) == 1 && abs(Y - h) == 1)
+                BlackAttack = 1;
+
+            break;
+        }
+        h--;
+        v--;
+    }
+
+    h = Y, v = X;
+    while (h >= 0 && v < deskSide) {
+        if (Desk[v][h] != ' ' && v != X && h != Y) {
+            if (Desk[v][h] == 'Q')
+                WhiteAttack = 1;
+            else if (Desk[v][h] == 'B')
+                WhiteAttack = 1;
+            else if (Desk[v][h] == 'P' && X - v == -1 && Y - h == 1)
+                WhiteAttack = 1;
+            else if (Desk[v][h] == 'K' && abs(X - v) == 1 && abs(Y - h) == 1)
+                WhiteAttack = 1;
+
+            if (Desk[v][h] == 'q')
+                BlackAttack = 1;
+            else if (Desk[v][h] == 'b')
+                BlackAttack = 1;
+            else if (Desk[v][h] == 'k' && abs(X - v) == 1 && abs(Y - h) == 1)
+                BlackAttack = 1;
+
+            break;
+        }
+        h--;
+        v++;
+    }
+    h = Y, v = X;
+    while (h < deskSide && v >= 0) {
+        if (Desk[v][h] != ' ' && v != X && h != Y) {
+            if (Desk[v][h] == 'Q')
+                WhiteAttack = 1;
+            else if (Desk[v][h] == 'B')
+                WhiteAttack = 1;
+            else if (Desk[v][h] == 'K' && abs(X - v) == 1 && abs(Y - h) == 1)
+                WhiteAttack = 1;
+
+            if (Desk[v][h] == 'q')
+                BlackAttack = 1;
+            else if (Desk[v][h] == 'b')
+                BlackAttack = 1;
+            else if (Desk[v][h] == 'p' && X - v == 1 && Y - h == -1)
+                BlackAttack = 1;
+            else if (Desk[v][h] == 'k' && abs(X - v) == 1 && abs(Y - h) == 1)
+                BlackAttack = 1;
+
+            break;
+        }
+        h++;
+        v--;
+    }
+    if (X - 2 >= 0 && Y - 1 >= 0) {
+        if (Desk[X - 2][Y - 1] == 'N')
+            WhiteAttack = 1;
+        else if (Desk[X - 2][Y - 1] == 'n')
+            BlackAttack = 1;
+    }
+    if (X + 2 < deskSide && Y - 1 >= 0) {
+        if (Desk[X + 2][Y - 1] == 'N')
+            WhiteAttack = 1;
+        else if (Desk[X + 2][Y - 1] == 'n')
+            BlackAttack = 1;
+    }
+    if (X - 2 >= 0 && Y + 1 < deskSide) {
+        if (Desk[X - 2][Y + 1] == 'N')
+            WhiteAttack = 1;
+        else if (Desk[X - 2][Y + 1] == 'n')
+            BlackAttack = 1;
+    }
+    if (X + 2 < deskSide && Y + 1 < deskSide) {
+        if (Desk[X + 2][Y + 1] == 'N')
+            WhiteAttack = 1;
+        else if (Desk[X + 2][Y + 1] == 'n')
+            BlackAttack = 1;
+    }
+
+    if (X - 1 >= 0 && Y - 2 >= 0) {
+        if (Desk[X - 1][Y - 2] == 'N')
+            WhiteAttack = 1;
+        else if (Desk[X - 1][Y - 2] == 'n')
+            BlackAttack = 1;
+    }
+    if (X + 1 < deskSide && Y - 2 >= 0) {
+        if (Desk[X + 1][Y - 2] == 'N')
+            WhiteAttack = 1;
+        else if (Desk[X + 1][Y - 2] == 'n')
+            BlackAttack = 1;
+    }
+    if (X - 1 >= 0 && Y + 2 < deskSide) {
+        if (Desk[X - 1][Y + 2] == 'N')
+            WhiteAttack = 1;
+        else if (Desk[X - 1][Y + 2] == 'n')
+            BlackAttack = 1;
+    }
+    if (X + 1 < deskSide && Y + 2 < deskSide) {
+        if (Desk[X + 1][Y + 2] == 'N')
+            WhiteAttack = 1;
+        else if (Desk[X + 1][Y + 2] == 'n')
+            BlackAttack = 1;
+    }
+    if (BlackAttack == 0)
+        return WhiteAttack;
+    if (WhiteAttack == 0)
+        return -BlackAttack;
+    return WhiteAttack + BlackAttack;
 }
 
 int* FindFigure(char Figure, char Desk[][deskSide])
