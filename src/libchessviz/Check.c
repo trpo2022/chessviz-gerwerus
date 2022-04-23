0ac2b45d30316d53a9259d68189787f337543d10
Герман Просянников <100204107+gerwerus@users.noreply.github.com>
2022-04-23 15:12:28 +0700

    Update Check.c

--- src/libchessviz/Check.c
+++ src/libchessviz/Check.c
@@ -12,52 +12,44 @@ Right CheckPawn(
     int Color = IsUpper(Figure);
     Right right;
     if (H0 == H1) {
-        if (Desk[V1][H1] != ' ')
-        {
+        if (Desk[V1][H1] != ' ') {
             right.Code = 1;
             strcpy(right.Error, "Pawn can't move like that");
             return right;
         };
-        if ((V0 - V1) * Color > 2 || (V0 - V1) * Color < 0)
-        {
+        if ((V0 - V1) * Color > 2 || (V0 - V1) * Color < 0) {
             right.Code = 1;
             strcpy(right.Error, "Pawn can't move like that");
             return right;
         };
-        if ((V0 - V1) * Color == 2 && (V0 != (deskSide - 1 - 1 * Color) % (deskSide - 1) || Desk[V0 - Color][H0] != ' '))
-        {
+        if ((V0 - V1) * Color == 2 && (V0 != (deskSide - 1 - 1 * Color) % (deskSide - 1) || Desk[V0 - Color][H0] != ' ')) {
             right.Code = 1;
             strcpy(right.Error, "Pawn can't move like that");
             return right;
         };
-        if (MoveType != '-')
-        {
+        if (MoveType != '-') {
             right.Code = 1;
             strcpy(right.Error, "Wrong move type");
             return right;
         };
     } else
     {
-        if (H1 - H0 != 1 && H0 - H1 != 1)
-        {
+        if (H1 - H0 != 1 && H0 - H1 != 1) {
             right.Code = 1;
             strcpy(right.Error, "Pawn can't move like that");
             return right;
         };
-        if ((V0 - V1) * Color != 1)
-        {
+        if ((V0 - V1) * Color != 1) {
             right.Code = 1;
             strcpy(right.Error, "Pawn can't move like that");
             return right;
         };
-        if (Desk[V1][H1] == ' ')
-        {
+        if (Desk[V1][H1] == ' ') {
             right.Code = 1;
             strcpy(right.Error, "Pawn can't move like that");
             return right;
         };
-        if (Color == IsUpper(Desk[V1][H1]))
-        {
+        if (Color == IsUpper(Desk[V1][H1])) {
             right.Code = 1;
             strcpy(right.Error, "Pawn can't move like that");
             return right;
